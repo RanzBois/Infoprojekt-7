d5c181879bdbca3c181b817ff7ff7c669e6d936b
info <info@DESKTOP-M228UUL.fritz.box>
2018-06-11 23:31:13 +0200

    Erste Version

---
+++ src/infoprojekt/InfoProjekt.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package infoprojekt;
+
+/**
+ *
+ * @author info
+ */
+public class InfoProjekt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Visuell f=new Visuell();
+        f.setVisible(true);
+    }
+    
+}
